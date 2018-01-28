c7bb5b9c5876514998b7ef6f36718eca2db538b2
chazzfit <basson_j@esme.fr>
2018-01-28 17:59:08 +0100

    draw  2D trajectory added

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import tkinter.filedialog
 from dicomConverter import dicom_converter
+from plotPoints import draw_trajectory
+
+
+pts = [(686, 324), (786, 374), (486, 474), (686, 343)]
+
 
 from_filename = tkinter.filedialog.askopenfilename()
-print (from_filename)
-dicom_converter(from_filename)
+if from_filename != ():
+    dicom_converter(from_filename)
+draw_trajectory(pts)
--- plotPoints.py
+++ plotPoints.py
@@ -1,13 +1,19 @@
 import cv2
 import numpy as np
 
-img = np.zeros((768, 1024, 3), dtype='uint8')
 
-points = np.array([[686, 344], [686, 344], [686, 344], [686, 343]])
-cv2.polylines(img, [points], 1, (255,255,255))
+# Draw the trajectory in 2D
+# Points must be an array of tuple each tuple contain the x and y coordinates
 
-winname = 'example'
-cv2.namedWindow(winname)
-cv2.imshow(winname, img)
-cv2.waitKey()
-cv2.destroyWindow(winname)
+def draw_trajectory(points):
+    img = np.zeros((1024, 1024, 3), dtype='uint8')
+    for j in range(0, len(points)):
+        if j != (len(points) - 1):
+            pt1 = points[j]
+            pt2 = points[j + 1]
+            cv2.line(img, pt1, pt2, (255, 0, 0), 3)
+
+    winname = 'trajectory'
+    cv2.namedWindow(winname)
+    cv2.imshow(winname, img)
+    cv2.waitKey()
