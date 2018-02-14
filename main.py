2d5da2f97bf07930e799695be655df5df58cd325
chazzfit <basson_j@esme.fr>
2018-02-14 11:26:10 +0100

    trajectory drawer + beizer smooth

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import tkinter.filedialog
 from dicomConverter import dicom_converter
-from plotPoints import draw_trajectory
+from plotPoints import draw_trajectory, lissage
 
 
-pts = [(686, 324), (786, 374), (486, 474), (686, 343)]
+pts = [(15, 15), (300, 50), (100, 500), (686, 324), (786, 374), (886, 474), (986, 543)]
 
 
-from_filename = tkinter.filedialog.askopenfilename()
-if from_filename != ():
-    dicom_converter(from_filename)
+# from_filename = tkinter.filedialog.askopenfilename()
+# if from_filename != ():
+
+# dicom_converter(from_filename)
+
+dicom_converter("")
+
+
+
+
 draw_trajectory(pts)
+ptx = [12, 100, 686, 786, 886, 986]
+pty = [15, 500, 324, 374, 474, 543]
+pts2 = [(100, 257), (686, 412), (786, 349), (886, 424)]
+
+# draw_trajectory(pts)
+# liste = lissage(pts, 1)
+#
+# draw_trajectory(liste)
--- plotPoints.py
+++ plotPoints.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
-
+import bezier
+from matplotlib import pyplot as plt
+from scipy.misc import comb
 
 # Draw the trajectory in 2D
 # Points must be an array of tuple each tuple contain the x and y coordinates
@@ -9,11 +11,67 @@ def draw_trajectory(points):
     img = np.zeros((1024, 1024, 3), dtype='uint8')
     for j in range(0, len(points)):
         if j != (len(points) - 1):
-            pt1 = points[j]
-            pt2 = points[j + 1]
-            cv2.line(img, pt1, pt2, (255, 0, 0), 3)
+            pt1 = (int(points[j][0]), int(points[j][1]))
+            pt2 = (int(points[j+1][0]), int(points[j+1][1]))
+            cv2.line(img, pt1, pt2, (255, 0, 0), 1)
 
     winname = 'trajectory'
     cv2.namedWindow(winname)
     cv2.imshow(winname, img)
-    cv2.waitKey()
+    cv2.waitKey(0)
+
+
+def lissage(points):
+
+    nPoints = len(points)
+    xpoints = [p[0] for p in points]
+    ypoints = [p[1] for p in points]
+
+    xvals, yvals = bezier_curve(points, nTimes=1000)
+    plt.plot(xvals, yvals)
+    plt.show()
+
+    plt.plot(xpoints, ypoints, "ro")
+    for nr in range(len(points)):
+        plt.text(points[nr][0], points[nr][1], nr)
+
+    plt.show()
+
+
+
+
+def bernstein_poly(i, n, t):
+    """
+     The Bernstein polynomial of n, i as a function of t
+    """
+
+    return comb(n, i) * ( t**(n-i) ) * (1 - t)**i
+
+
+def bezier_curve(points, nTimes):
+    print(points)
+    """
+       Given a set of control points, return the
+       bezier curve defined by the control points.
+
+       points should be a list of lists, or list of tuples
+       such as [ [1,1],
+                 [2,3],
+                 [4,5], ..[Xn, Yn] ]
+        nTimes is the number of time steps, defaults to 1000
+
+        See http://processingjs.nihongoresources.com/bezierinfo/
+    """
+
+    nPoints = len(points)
+    xPoints = np.array([p[0] for p in points])
+    yPoints = np.array([p[1] for p in points])
+
+    t = np.linspace(0.0, 1.0, nTimes)
+
+    polynomial_array = np.array([ bernstein_poly(i, nPoints-1, t) for i in range(0, nPoints)   ])
+
+    xvals = np.dot(xPoints, polynomial_array)
+    yvals = np.dot(yPoints, polynomial_array)
+
+    return xvals, yvals
