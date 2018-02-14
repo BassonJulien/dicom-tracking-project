37d065110349605f9a0537995fd2cc1c4e73f230
camelot <caminalel@gmail.com>
2018-02-14 12:07:59 +0100

    file cached erased

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
-import tkinter.filedialog
-from dicomConverter import dicom_converter
-from plotPoints import draw_trajectory, lissage
-
-
-pts = [(15, 15), (300, 50), (100, 500), (686, 324), (786, 374), (886, 474), (986, 543)]
-
-
-# from_filename = tkinter.filedialog.askopenfilename()
-# if from_filename != ():
-
-# dicom_converter(from_filename)
-
-dicom_converter("")
-
-
-
-
-draw_trajectory(pts)
-ptx = [12, 100, 686, 786, 886, 986]
-pty = [15, 500, 324, 374, 474, 543]
-pts2 = [(100, 257), (686, 412), (786, 349), (886, 424)]
-
-# draw_trajectory(pts)
-# liste = lissage(pts, 1)
+# import tkinter.filedialog
+# from dicomConverter import dicom_converter
+# from plotPoints import draw_trajectory, lissage
 #
-# draw_trajectory(liste)
-# import dicom
-# import numpy as np
-# import cv2
-# import orbDetection
-# 
-# file_name = '/home/camelot/Vidéos/angios/test1.DCM'
-# if __name__ == '__main__':
-#     # Data header of the dicom file
-#     data_dicom = dicom.read_file(file_name)
-#     img_dicom = np.array(data_dicom.pixel_array[0], np.uint8)
-#     orbDetection.main_orb_detection(img_dicom)
+#
+# pts = [(15, 15), (300, 50), (100, 500), (686, 324), (786, 374), (886, 474), (986, 543)]
+#
+#
+# # from_filename = tkinter.filedialog.askopenfilename()
+# # if from_filename != ():
+#
+# # dicom_converter(from_filename)
+#
+# dicom_converter("")
+#
+#
+#
+#
+# draw_trajectory(pts)
+# ptx = [12, 100, 686, 786, 886, 986]
+# pty = [15, 500, 324, 374, 474, 543]
+# pts2 = [(100, 257), (686, 412), (786, 349), (886, 424)]
+#
+# # draw_trajectory(pts)
+# # liste = lissage(pts, 1)
+# #
+# # draw_trajectory(liste)
+import dicom
+import numpy as np
+import cv2
+import orbDetection
+
+file_name = '/home/camelot/Vidéos/angios/test1.DCM'
+if __name__ == '__main__':
+    # Data header of the dicom file
+    data_dicom = dicom.read_file(file_name)
+    img_dicom = np.array(data_dicom.pixel_array[0], np.uint8)
+    orbDetection.main_orb_detection(img_dicom)
